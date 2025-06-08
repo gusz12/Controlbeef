14994f5357b5d61582ff36aa7f64c763dce3c6ff
LCP05 <lucas.previtero@sptech.school>
2025-06-08 19:59:49 -0300

    Populamento dos dados do banco

--- src/database/ScriptControlBeef.sql
+++ src/database/ScriptControlBeef.sql
@@ -1,13 +1,18 @@
-    CREATE DATABASE IF NOT EXISTS ControlBeef;
+CREATE DATABASE IF NOT EXISTS ControlBeef;
 USE ControlBeef;
 
+
+
+-- -------------------------------------------------------------------------------------
+-- Tabelas de endereco
 CREATE TABLE estado(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nomeE VARCHAR(70),
     sigla CHAR(2)
 );
-INSERT INTO estado (nomeE, sigla) VALUES 
+INSERT INTO estado (nomeE, sigla) VALUES
 ('São Paulo', 'SP');
+
 
 
 
@@ -18,8 +23,8 @@ CREATE TABLE cidade(
     CONSTRAINT fkestado_cidade FOREIGN KEY (fkestado) REFERENCES estado(id)
 );
 INSERT INTO cidade (nomeC, fkestado) VALUES 
-('São Paulo', 1),
-('Campinas', 1);
+('São Paulo', 1);
+
 
 
 
@@ -34,24 +39,42 @@ CREATE TABLE endereco(
     CONSTRAINT fk_cidade_endereco FOREIGN KEY (fkcidade) REFERENCES cidade(id)
 );
 INSERT INTO endereco (logradouro, cep, numero, complemento, bairro, fkcidade) VALUES 
-('Rua das Carnes', '01010100', 123, '10º andar','Centro', 1),
-('Rua Congelada', '30120100', 789, '1º andar', 'Frio', 1);
+('Avenida Marginal Direita do Tietê', '05118100', 500, null, 'Vila Jaguara', 1),
+('Avenida Queiroz Filho,', '05319000', 1560, null, 'Vila Hamburguesa', 1),
+('Rua Doutor Gabriel de Oliveira Rocha', '18681030', 704, null, 'Vila Maria Cristina', 1);
 
 
+INSERT INTO endereco (logradouro, cep, numero, complemento, bairro, fkcidade) VALUES 
+('Av. Central 1500', '04567890', 1500, 'Setor 1', 'Jardim Central', 1),        
+('Rua do Frio 300', '04567891', 300, 'Bloco C', 'Vila Frigorífica', 1),            
 
+('Rodovia das Carnes 50', '07890123', 50, 'Lote 5', 'Distrito Agroindustrial', 1),  
+('Rua da Fábrica 400', '07890124', 400, 'Galpão D', 'Vila Industrial', 1),        
+
+('Av. Frigorífico 900', '03456789', 900, 'Galpão E', 'Bairro Industrial', 1),      
+('Rua dos Carnes 250', '03456790', 250, 'Bloco F', 'Vila Frigorífica', 1); 
+
+-- -------------------------------------------------------------------------------------
+-- Tabelas gerenciamento usuários
 
 CREATE TABLE empresa(
     id INT PRIMARY KEY AUTO_INCREMENT,
     razao_social VARCHAR(50),
     fkendereco INT,
     representante VARCHAR(40),
-    telefone CHAR(10),
+    telefone VARCHAR(11),
     cnpj CHAR(14),
     codigo_ativacao VARCHAR(50),
     CONSTRAINT fkendereco_empresa FOREIGN KEY (fkendereco) REFERENCES endereco(id)
 );
 INSERT INTO empresa (razao_social, fkendereco, representante, telefone, cnpj, codigo_ativacao) VALUES 
-('Friboi Ltda.', 1, 'João Silva', '1199999999', '12345678912345', 'CBCODElm35');
+('ControlBeef', 1, 'Lucas Canuto Previtero', '11953101496', '47697046822', 'CBCODElm35'),
+('Friboi Ltda.', 2, 'João Silva', '1131444000', '02916265002707', 'CBCODE1lm35'),
+('Marfrig', 3, 'Ednaldo Pereira', '1137928653', '03853896000140', 'CBCODE2lm35'),
+('Frigol Indústria de Alimentos Ltda', 4, 'Carlos Silva', '11987654321', '12345678000199', 'CBCODE3lm35');
+
+
+
 
 
 
@@ -64,8 +87,137 @@ CREATE TABLE usuario(
     CONSTRAINT fkEmpresa_usuario FOREIGN KEY (fkempresa) REFERENCES empresa(id)
 );
 INSERT INTO usuario (nome, email, senha, fkempresa) VALUES 
-('Administrador', 'admin@controlbeef.com', 'senha123', 1),
-('Técnico Sensorial', 'tecnico@controlbeef.com', 'sensor456',  1);
+('ADMFriboi', 'admin@friboi.com', 'senha123', 1),
+('ADMMarfrig', 'admin@marfrig.com', 'senha123', 2),
+('ADMFrigol', 'admin@frigol.com', 'senha123', 3);
+
+INSERT INTO usuario (nome, email, senha, fkempresa) VALUES 
+('Celina Benedito', 'celina.benedito@sptech.school', '123', 1),
+('Gabriel Rapani', 'gabriel.rapani@sptech.school', '123',  1), 
+('Vitorio Bearari', 'vitorio.bearari@sptech.school', '123', 1),
+('Gustavo Menezes', 'gustavo.menezes@sptech.school', '123', 1),
+('Lucas Previtero', 'lucas.previtero@sptech.school', '123',  1);
+
+
+
+
+-- -------------------------------------------------------------------------------------
+-- Tabelas gerenciamentos frigoríficos
+CREATE TABLE frigorifico(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nomeFrigo VARCHAR(30),
+    fkendereco INT,
+    fkempresa INT,
+    CONSTRAINT fkEmpresa_frigorifico FOREIGN KEY (fkempresa) REFERENCES empresa(id),
+    CONSTRAINT fkendereco_frigorifico FOREIGN KEY (fkendereco) REFERENCES endereco(id)
+);
+INSERT INTO frigorifico (nomeFrigo, fkendereco, fkempresa) VALUES 
+('Friboi Frigorífico 1', 4, 2),
+('Friboi Frigorífico 2', 5, 2),
+
+('Marfrig Frigorífico 1', 6, 3),
+('Marfrig Frigorífico 2', 7, 3),
+
+('Frigol Frigorífico 1', 8, 4),
+('Frigol Frigorífico 2', 9, 4);
+
+
+
+CREATE TABLE salas_frias(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nomeSala VARCHAR(30),
+    fkfrigo INT,
+    setor VARCHAR(30),
+    CONSTRAINT fkFrigorifico_salasFrias FOREIGN KEY (fkfrigo) REFERENCES frigorifico(id)
+);
+INSERT INTO salas_frias(nomeSala, fkfrigo, setor) VALUES
+('Sala 1', 1, 1),
+('Sala 2', 1, 1),
+('Sala 3', 1, 2),
+('Sala 4', 1, 2),
+
+('Sala 1', 2, 1),
+('Sala 2', 2, 1),
+('Sala 3', 2, 2),
+('Sala 4', 2, 2),
+
+('Sala 1', 3, 1),
+('Sala 2', 3, 1),
+('Sala 3', 3, 2),
+('Sala 4', 3, 2);
+
+
+
+
+CREATE TABLE sensor(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fkSala INT,
+    CONSTRAINT fksala_sensor FOREIGN KEY (fkSala) REFERENCES salas_frias(id)
+);
+
+/*select * from sensor s inner join salas_frias sf on sf.id = s.fkSala
+inner join frigorifico f on sf.fkFrigorifico = f.id;*/
+
+INSERT INTO sensor (fkSala) VALUES 
+(1),
+(1),
+(2),
+(2),
+(3),
+(3),
+(4),
+(4),
+(5),
+(5),
+(6),
+(6),
+(7),
+(7),
+(8),
+(8),
+(9),
+(9),
+(10),
+(10),
+(11),
+(11),
+(12),
+(12);
+
+
+
+CREATE TABLE dados(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fksensor INT DEFAULT 1,
+    sensor_analogico DECIMAL(10,2),
+    data_medicao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fksensor_dados FOREIGN KEY (fksensor) REFERENCES sensor(id)
+);
+INSERT INTO dados (fksensor, sensor_analogico) VALUES
+(1, -2.5),
+(2, -3.0),
+(3, 0.5),
+(4, 1.2),
+(5, -4.7),  
+(6, 5.3),    
+(7, 2.0),
+(8, -2.2),
+(9, 3.8),
+(10, 4.1),  
+(11, -3.2),  
+(12, -10.5), 
+(13, 3.4),
+(14, 3.9),
+(15, 0.0),
+(16, 1.8),
+(17, 6.0),  
+(18, 7.8),  
+(19, -1.3),
+(20, -2.8),
+(21, 3.5),
+(22, 2.9),
+(23, -8.6),  
+(24, 0.7);
 
 
 
@@ -78,65 +230,190 @@ CREATE TABLE aviso (
 );
 
 
-
-
-CREATE TABLE frigorifico(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nomeFrigo VARCHAR(30),
-    fkendereco INT,
-    fkempresa INT,
-    CONSTRAINT fkEmpresa_frigorifico FOREIGN KEY (fkempresa) REFERENCES empresa(id),
-    CONSTRAINT fkendereco_frigorifico FOREIGN KEY (fkendereco) REFERENCES endereco(id)
-);
-INSERT INTO frigorifico (nomeFrigo, fkendereco, fkempresa) VALUES 
-('Frigorífico Central', 1, 1),
-('Frigo Norte', 2, 1);
+/*
+-- Dados para teste
+insert into dados(fksensor, sensor_analogico, data_medicao)
+values
+(1, 4.00, '2025-06-06'),
+(1, 2.00, '2025-06-06'),
+(1, 5.00, '2025-06-06'),
+(1, 1.00, '2025-06-06'),
+(1, 0.00, '2025-06-06');
 
 
 
 
 
 
-CREATE TABLE salas_frias(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nomeSala VARCHAR(30),
-    fkfrigo INT,
-    setor VARCHAR(30),
-    CONSTRAINT fkFrigorifico_salasFrias FOREIGN KEY (fkfrigo) REFERENCES frigorifico(id)
-);
-INSERT INTO salas_frias (nomeSala, fkfrigo, setor) VALUES 
-('Sala 1', 1, 1),
-('Sala 2', 1, 2);
+-- 1. Inserir estados
+INSERT INTO estado (nomeE, sigla) VALUES
+('São Paulo', 'SP'),
+('Minas Gerais', 'MG');
+
+-- 2. Inserir cidades
+INSERT INTO cidade (nomeC, fkestado) VALUES
+('São Paulo', 1),
+('Belo Horizonte', 2);
+
+-- 3. Inserir endereços
+INSERT INTO endereco (logradouro, cep, numero, complemento, bairro, fkcidade) VALUES
+('Rua A', '01001000', 123, 'Apto 1', 'Centro', 1),
+('Rua B', '30130000', 456, 'Casa', 'Savassi', 2);
+
+-- 4. Inserir empresas
+INSERT INTO empresa (razao_social, fkendereco, representante, telefone, cnpj, codigo_ativacao) VALUES
+('Empresa SP', 1, 'João Silva', '1123456789', '12345678000199', 'ABC123'),
+('Empresa MG', 2, 'Maria Souza', '3134567890', '98765432000188', 'XYZ789');
+
+-- 5. Inserir frigoríficos
+INSERT INTO frigorifico (nomeFrigo, fkendereco, fkempresa) VALUES
+('Frigorífico SP', 1, 1),
+('Frigorífico MG', 2, 2);
+
+-- 6. Inserir 4 salas para o Frigorífico SP (id = 1)
+INSERT INTO salas_frias (nomeSala, fkfrigo, setor) VALUES
+('Sala 1A', 1, 'Setor A'),
+('Sala 1B', 1, 'Setor B'),
+('Sala 1C', 1, 'Setor C'),
+('Sala 1D', 1, 'Setor D');
+
+-- 7. Inserir 4 salas para o Frigorífico MG (id = 2)
+*/
 
 
 
-CREATE TABLE sensor(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fkSala INT,
-    CONSTRAINT fksala_sensor FOREIGN KEY (fkSala) REFERENCES salas_frias(id)
-);
-INSERT INTO sensor (fkSala) VALUES 
-(1);
 
 
 
 
-CREATE TABLE dados(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    fksensor INT DEFAULT 1,
-    sensor_analogico DECIMAL(10,2),
-    data_medicao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fksensor_dados FOREIGN KEY (fksensor) REFERENCES sensor(id)
-);
+-- Select para para os cards 
+    select sf.fkfrigo,
+    f.nomeFrigo,
+  
+    sf.nomeSala,
+    s.id as número_sensor,
+    d.sensor_analogico,
+        case 
+        when sensor_analogico > 4.0 then 'Crítico'
+        when sensor_analogico < -3.0 then 'Crítico'
+        when sensor_analogico = -3.0 then 'Alerta'
+        when sensor_analogico = 4.0 then 'Alerta'
+        else 'Ideal'
+    end as Status_alerta,
+    d.data_medicao
+
+    from frigorifico f
+    inner join salas_frias sf on f.id = sf.fkfrigo
+    inner join sensor s on s.fkSala = sf.id
+    inner join dados d on d.fksensor = s.id
+	where f.fkempresa = 2 and f.id = 1
+    
+    group by f.nomeFrigo, sf.nomeSala, s.id, d.sensor_analogico, Status_alerta, d.data_medicao
+    order by 
+    case 
+        when Status_alerta = 'Crítico' then 1
+        when Status_alerta = 'Alerta' then 2
+        else 3
+    end;
 
 
 
 
 
+  select 
+e.id as idEmpresa, e.razao_social as nomeEmpresa, e.representante,
+f.id as idFrigorifico, f.nomeFrigo as nomeFrigorifico, f.fkempresa,
+s.id as idSala, s.nomeSala, so.id as  idSensor, so.fkSala, d.    id as idDados,
+d.fksensor, d.sensor_analogico as dadosSensor, d.data_medicao as dataMedicao
+from empresa e
+inner join frigorifico f on e.id = f.fkempresa
+inner join salas_frias s on f.id = s.fkfrigo
+inner join sensor so on s.id = so.fkSala
+inner join dados d on so.id=d.fksensor
+where f.fkempresa = 2 and f.id = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+-- Ranking de frigorificos maiores médias fora do ideal
+select
+	sf.fkfrigo,
+    f.nomeFrigo,
+    s.fkSala as número_sala,
+    sf.nomeSala,
+    avg(d.sensor_analogico) as média_sala
+	from frigorifico f
+    inner join salas_frias sf on f.id = sf.fkfrigo
+    inner join sensor s on s.fkSala = sf.id
+    inner join dados d on d.fksensor = s.id
+	where f.fkempresa = 2 and f.id = 2
+    group by sf.fkfrigo,
+    f.nomeFrigo,
+    s.fkSala,
+    sf.nomeSala
+    having média_sala > 4 or média_sala < -3
+;
+
+-- Nome sala 
+-- Quantidade de vezes fora da temperatura ideal 
+-- Tempo fora do ideal
+-- Mostrar vezes fora do ideal e tempo fora do ideal 
+use Controlbeef;
+select 
+sf.fkfrigo as Numero_Frigo,
+sf.nomeSala,
+concat(
+count(
+	case 
+		when d.sensor_analogico > 4 then 1
+        when d.sensor_analogico < -3 then 1
+		else null
+	end
+)*2, ' Segundos') as Tempo_Fora_Ideal
+from salas_frias sf
+inner join frigorifico f on f.id = sf.fkfrigo
+inner join sensor s on s.fkSala = sf.id
+inner join dados d on d.fksensor = s.id
+where f.fkempresa = 2 and f.id = 1
+group by sf.fkfrigo, sf.nomeSala;
+
+-- Feito pelo chat
+SELECT 
+    sf.fkfrigo AS Numero_Frigo,
+    sf.nomeSala,
+    COUNT(
+        CASE 
+            WHEN d.sensor_analogico > 4 THEN 1
+            WHEN d.sensor_analogico < -3 THEN 1
+            ELSE NULL
+        END
+    ) AS Quantidade_Vezes_Fora_Ideal
+FROM salas_frias sf
+INNER JOIN frigorifico f ON f.id = sf.fkfrigo
+INNER JOIN sensor s ON s.fkSala = sf.id
+INNER JOIN dados d ON d.fksensor = s.id
+WHERE f.fkempresa = 1 AND f.id = 1
+GROUP BY sf.fkfrigo, sf.nomeSala;
+select * from salas_frias;
+select * from dados;
+select * from sensor;
+
+-- select de verificação
+select * from frigorifico join salas_frias join sensor join dados;
 -- verificar se a empresa tem o frigorifigo
 select 
 e.id,
 e.razao_social,
 f.id,
 f.nomeFrigo
-from empresa
+from empresa e
+left join frigorifico f on e.id = f.fkempresa 
+where f.fkempresa = 2;
